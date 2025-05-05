3319159d96e9ac9a7021b4e9f90e9130042e236b
putthipong-kiti <putthipong.kiti@gmail.com>
2025-05-05 21:00:28 +0700

    edit test file

--- spec/system/user_can_delete_a_room_spec.rb
+++ spec/system/user_can_delete_a_room_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "User can delete a room", type: :system do
-  before do
-    Room.create!(name: "Test Room")
+describe "User can delete a room" do
+  context "when user create a room" do
+    before do
+      Room.create!(name: "Test Room")
+    end
+    it "allow user to deletes a room" do
+      user_visits_rooms_path
+      user_clicks_delete_room
+      user_must_see_delete_message
+    end
   end
 
-  it "deletes a room" do
+  def user_visits_rooms_path
     visit rooms_path
+  end
+
+  def user_clicks_delete_room
     click_button 'Delete Room'
+  end
+
+  def user_must_see_delete_message
     expect(page).to have_content 'Room was successfully deleted.'
   end
 end
